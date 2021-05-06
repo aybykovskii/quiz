ea2c445d1b1dd1b1ba76d593f621917c8a7ac9eb
kikojj <vladislav.bykovskiy@gmail.com>
2021-05-06 16:45:27 +1000

    mistakes fix2

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import { useStyle } from "./style";
 /*
   NOTE:
     Тут ты видимо не понял, как типизировать children и сделал так.
-      childre: React.ReactNode
+      children: React.ReactNode
     Но на самом деле, ты их можешь даже не указывать, если укажешь, что Layout: React.FC.
     Потому что по дефолту в пропсах React.FC есть проп children
 */
